bc1d64522afa110787767b34fb9fecd8314fa853
Victor Noël <victor.noel@crazydwarves.org>
2019-09-15 12:31:54 +0200

    (#133) Add MatchesBefore matcher to replace JUnit's timeout

---
+++ src/main/java/org/llorllale/cactoos/matchers/MatchesBefore.java
@@ -0,0 +1,68 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) for portions of project cactoos-matchers are held by
+ * Yegor Bugayenko, 2017-2018, as part of project cactoos.
+ * All other copyright for project cactoos-matchers are held by
+ * George Aristy, 2018.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.llorllale.cactoos.matchers;
+
+import org.cactoos.func.Timed;
+import org.hamcrest.Matcher;
+
+/**
+ * Matcher to check that scalar finishes before some timeout.
+ *
+ * This is a {@link Matcher} alternative to JUnit's {@link Test#timeout}.
+ *
+ * <p>Here is an example how {@link MatchesBefore} can be used:</p>
+ * <pre>
+ *  new Assertion<>(
+ *       "must run in 5 seconds maximum",
+ *       new TextOf("test"),
+ *       new MatchesBefore(
+ *           5000,
+ *           new TextIs("test")
+ *       )
+ *  ).affirm();</pre>
+ *
+ * @param <T> Type of the scalar's value
+ * @since 1.0.0
+ */
+public final class MatchesBefore<T> extends MatcherEnvelope<T> {
+    /**
+     * Ctor.
+     * @param millisec Timeout.
+     * @param matcher Matcher.
+     */
+    public MatchesBefore(final long millisec, final Matcher<T> matcher) {
+        super(
+        // @checkstyle IndentationCheck (6 line)
+        new Timed<>(matcher::matches, millisec),
+        desc -> desc
+            .appendDescriptionOf(matcher)
+            .appendText(" runs in less than ")
+            .appendValue(millisec).appendText(" milliseconds"),
+        matcher::describeMismatch
+        );
+    }
+}
---
+++ src/test/java/org/llorllale/cactoos/matchers/MatchesBeforeTest.java
@@ -0,0 +1,94 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) for portions of project cactoos-matchers are held by
+ * Yegor Bugayenko, 2017-2018, as part of project cactoos.
+ * All other copyright for project cactoos-matchers are held by
+ * George Aristy, 2018.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.llorllale.cactoos.matchers;
+
+import org.cactoos.Text;
+import org.cactoos.text.TextOf;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Test case for {@link MatchesBefore}.
+ *
+ * @since 1.0.0
+ * @checkstyle JavadocMethodCheck (500 lines)
+ * @checkstyle MagicNumberCheck (500 line)
+ */
+public final class MatchesBeforeTest {
+
+    /**
+     * Example of {@link MatchesBefore} usage.
+     */
+    @Test
+    public void matches() {
+        final String val = "test";
+        new Assertion<>(
+            "must run in 1000 milliseconds maximum",
+            new TextOf(val),
+            new MatchesBefore<>(
+                1000,
+                new TextIs(val)
+            )
+        ).affirm();
+    }
+
+    @Test
+    public void mismatchesFromMatcher() {
+        new Assertion<>(
+            "must fail because of matcher",
+            new MatchesBefore<>(1000, new TextIs("a")),
+            new Mismatches<>(
+                new TextOf("b"),
+                "Text with value \"a\" runs in less than <1000L> milliseconds",
+                "Text is \"b\""
+            )
+        ).affirm();
+    }
+
+    // @todo #133:30min Currently MatchesBefore directly throws a
+    //  TimeoutException but instead it should populate the mismatch
+    //  message so that it can be properly tested with Mismatches.
+    //  Unignore and update this test to validate it is correctly implemented
+    @Ignore
+    @Test
+    public void mismatchesFromTimeout() {
+        final String val = "c";
+        new Assertion<>(
+            "must fail because of timeout",
+            new MatchesBefore<>(10, new TextIs(val)),
+            new Mismatches<>(
+                (Text) () -> {
+                    Thread.sleep(1000);
+                    return val;
+                },
+                "Text with value \"c\" runs in less than <10L> milliseconds",
+                "Tto be determined"
+            )
+        ).affirm();
+    }
+}
