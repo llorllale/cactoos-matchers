cf6c44e76602a9c08ccfee8f4c578109b0232f8e
andreoss <andreoss@sdf.org>
2021-05-08 23:02:36 -0400

    (#235) Add `IsEqualTo`, `IsGreaterThan`, `IsLessThan` & etc.

---
+++ src/test/java/org/llorllale/cactoos/matchers/IsLessThanTest.java
@@ -0,0 +1,62 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) for portions of project cactoos-matchers are held by
+ * Yegor Bugayenko, 2017-2018, as part of project cactoos.
+ * All other copyright for project cactoos-matchers are held by
+ * George Aristy, 2018-2020.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.llorllale.cactoos.matchers;
+
+import org.hamcrest.core.AllOf;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for {@link IsLessThan}.
+ * @since 1.0.0
+ */
+@SuppressWarnings("unchecked")
+final class IsLessThanTest {
+    @Test
+    void matches() {
+        new Assertion<>(
+            "Must match",
+            Integer.MIN_VALUE,
+            new AllOf<>(
+                new IsLessThan<>(0),
+                new IsLessThan<>(-1)
+            )
+        ).affirm();
+    }
+
+    @Test
+    void mismatches() {
+        new Assertion<>(
+            "Must mismatch",
+            new IsLessThan<>(0),
+            new Mismatches<>(
+                1,
+                "a value less than <0> when compared by <NaturalOrdering{}>",
+                "<1> was greater than <0> when compared by <NaturalOrdering{}>"
+            )
+        ).affirm();
+    }
+}
